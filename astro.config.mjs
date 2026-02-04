f20054880eff0e003d5b2ac655edb379697630d6
Developer <dev@ultimateducation.local>
2026-02-04 11:48:56 +0700

    vercel server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,8 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  output: 'hybrid',
+  output: 'static',
   integrations: [tailwind(), react()],
-  adapter: node({
-    mode: 'standalone'
-  }),
-  server: {
-    port: 3000,
-    host: true
-  }
 });
