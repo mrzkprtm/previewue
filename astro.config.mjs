99f2c50051aef89bbc4c4f154d6df55e8f7ce525
Developer <mrzkp05@gmail.com>
2026-02-08 11:50:42 +0700

    fix: switch to vercel adapter to resolve 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: 'hybrid',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: vercel(),
   integrations: [tailwind(), react()],
 });
