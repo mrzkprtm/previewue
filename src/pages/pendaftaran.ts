f5fca18cadf4b15453834b50a65ef9feb649f08b
Developer <mrzkp05@gmail.com>
2026-02-19 16:39:36 +0700

    daftars

--- src/pages/pendaftaran.ts
+++
@@ -1,9 +0,0 @@
-import type { APIRoute } from "astro";
-import pendaftaranHtml from "../../pendaftaran.html?raw";
-
-export const GET: APIRoute = async () => {
-    return new Response(pendaftaranHtml, {
-        headers: { "content-type": "text/html; charset=utf-8" },
-        status: 200,
-    });
-};
